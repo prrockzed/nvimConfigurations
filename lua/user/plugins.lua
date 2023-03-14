761f78a097f6e8815d9d6d614e7d5d0891233e82
prrockzed <prrockzed@pm.me>
2023-03-14 15:54:13 +0530

    Modified plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,6 +44,22 @@ return packer.startup(function(use)
 
  use { "wbthomason/packer.nvim" } -- Have packer manage itself
  use { "nvim-lua/plenary.nvim" } -- Useful lua functions used by lots of plugins
+ use { "windwp/nvim-autopairs" } -- Autopairs, integrates with both cmp and treesitter
+ --  use { "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
+ --  use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
+ --  use { "akinsho/bufferline.nvim", commit = "83bf4dc7bff642e145c8b4547aa596803a8b4dc4" }
+ --	use { "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" }
+ --  use { "nvim-lualine/lualine.nvim", commit = "a52f078026b27694d2290e34efa61a6e4a690621" }
+ --  use { "akinsho/toggleterm.nvim", commit = "2a787c426ef00cb3488c11b14f5dcf892bbd0bda" }
+ --  use { "ahmedkhalf/project.nvim", commit = "628de7e433dd503e782831fe150bb750e56e55d6" }
+ --  use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
+ --  use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
+ --  use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
+ --	use {"folke/which-key.nvim"}
+
+ -- Commenting
+ use { "numToStr/Comment.nvim" }
+ use { "JoosepAlviste/nvim-ts-context-commentstring" }
 
 
 
@@ -57,21 +73,6 @@ return packer.startup(function(use)
 
 
 
-
- --  use { "windwp/nvim-autopairs", commit = "4fc96c8f3df89b6d23e5092d31c866c53a346347" } -- Autopairs, integrates with both cmp and treesitter
- --  use { "numToStr/Comment.nvim", commit = "97a188a98b5a3a6f9b1b850799ac078faa17ab67" }
- --  use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "4d3a68c41a53add8804f471fcc49bb398fe8de08" }
- --  use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
- --  use { "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
- --  use { "akinsho/bufferline.nvim", commit = "83bf4dc7bff642e145c8b4547aa596803a8b4dc4" }
- --	use { "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" }
- --  use { "nvim-lualine/lualine.nvim", commit = "a52f078026b27694d2290e34efa61a6e4a690621" }
- --  use { "akinsho/toggleterm.nvim", commit = "2a787c426ef00cb3488c11b14f5dcf892bbd0bda" }
- --  use { "ahmedkhalf/project.nvim", commit = "628de7e433dd503e782831fe150bb750e56e55d6" }
- --  use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
- --  use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
- --  use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
- --	use {"folke/which-key.nvim"}
 
  -- Colorschemes
  use { "folke/tokyonight.nvim"}
@@ -93,23 +94,22 @@ return packer.startup(function(use)
  use { "L3MON4D3/LuaSnip", run = "make install_jsregexp" }
  use { "rafamadriz/friendly-snippets" } -- a bunch of snippets to use
 
- --	-- LSP
+ -- LSP
  use { "neovim/nvim-lspconfig" } -- enable LSP
  use { "williamboman/mason.nvim" } -- simple to use language server installer
  use { "williamboman/mason-lspconfig.nvim" }
  use { "jose-elias-alvarez/null-ls.nvim" } -- for formatters and linters
  --  use "RRethy/vim-illuminate"
 
- --	-- Telescope
- --	use { "nvim-telescope/telescope.nvim", commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" }
+ -- Telescope
+ use { "nvim-telescope/telescope.nvim", tag = '0.1.1' }
 
- --	-- Treesitter
- --	use {
- --		"nvim-treesitter/nvim-treesitter",
- --		commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
- --	}
+ -- Treesitter
+ use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+ use { "HiPhish/nvim-ts-rainbow2"} -- rainbow highlighting
+ use { "nvim-treesitter/playground" }
 
- --	-- Git
+ -- Git
  --	use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
 
  --  -- Wakatime
