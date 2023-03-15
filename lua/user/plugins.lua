c7fc58576db88748d976cf848468dca24be44300
prrockzed <prrockzed@pm.me>
2023-03-15 15:00:37 +0530

    Modified whichkey files according to myself and hence updated keymaps

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -38,82 +38,74 @@ packer.init({
 	},
 })
 
-
 -- Install your plugins here
 return packer.startup(function(use)
+	use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
+	use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
+	use({ "windwp/nvim-autopairs" }) -- Autopairs, integrates with both cmp and treesitter
+	use({ "kyazdani42/nvim-tree.lua" })
+	use({ "kyazdani42/nvim-web-devicons" })
+	use({ "akinsho/bufferline.nvim" })
+	use({ "moll/vim-bbye" })
+	use({ "nvim-lualine/lualine.nvim" })
+	use({ "akinsho/toggleterm.nvim" })
+	use({ "ahmedkhalf/project.nvim" })
+	use({ "lewis6991/impatient.nvim" })
+	use({ "lukas-reineke/indent-blankline.nvim" })
+	use({ "goolord/alpha-nvim" })
+	use({ "folke/which-key.nvim" })
 
- use { "wbthomason/packer.nvim" } -- Have packer manage itself
- use { "nvim-lua/plenary.nvim" } -- Useful lua functions used by lots of plugins
- use { "windwp/nvim-autopairs" } -- Autopairs, integrates with both cmp and treesitter
- use { "kyazdani42/nvim-tree.lua" }
- use { "kyazdani42/nvim-web-devicons" }
- use { "akinsho/bufferline.nvim" }
- use { "moll/vim-bbye"}
- use { "nvim-lualine/lualine.nvim" }
- use { "akinsho/toggleterm.nvim" }
- use { "ahmedkhalf/project.nvim" }
- use { "lewis6991/impatient.nvim" }
- use { "lukas-reineke/indent-blankline.nvim" }
- use { "goolord/alpha-nvim" }
- use {"folke/which-key.nvim" }
+	-- Commenting
+	use({ "numToStr/Comment.nvim" })
+	use({ "JoosepAlviste/nvim-ts-context-commentstring" })
 
- -- Commenting
- use { "numToStr/Comment.nvim" }
- use { "JoosepAlviste/nvim-ts-context-commentstring" }
+	-- Extra Plugins
+	use({ "nvim-lua/popup.nvim" }) -- (Was not here originally)An implementation of the Popup Api from vim in neovim
+	use({ "rstacruz/vim-closer" }) -- (Was not here originally)
+	-- use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}  -- (Was not here originally)
+	-- use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'} -- (Was not here originally)
+	-- use {'andymass/vim-matchup', event = 'VimEnter'} -- (Was not here originally)
 
+	-- Colorschemes
+	use({ "folke/tokyonight.nvim" })
+	use({ "lunarvim/darkplus.nvim" })
+	use({ "lunarvim/colorschemes" })
+	use({ "rktjmp/lush.nvim" })
+	use({ "metalelf0/jellybeans-nvim" })
+	use({ "EdenEast/nightfox.nvim" }) -- Packer
+	-- Cmp
+	use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
+	use({ "hrsh7th/cmp-buffer" }) -- buffer completions
+	use({ "hrsh7th/cmp-path" }) -- path completions
+	use({ "hrsh7th/cmp-cmdline" }) -- cmdline completions
+	use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
+	use({ "hrsh7th/cmp-nvim-lsp" }) -- lsp configuration
+	use({ "hrsh7th/cmp-nvim-lua" }) -- lua configuration
 
+	-- Snippets
+	use({ "L3MON4D3/LuaSnip", run = "make install_jsregexp" })
+	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
 
- -- Extra Plugins
- use { "nvim-lua/popup.nvim" }  -- (Was not here originally)An implementation of the Popup Api from vim in neovim
- use { "rstacruz/vim-closer" } -- (Was not here originally)
- -- use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}  -- (Was not here originally)
- -- use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'} -- (Was not here originally)
- -- use {'andymass/vim-matchup', event = 'VimEnter'} -- (Was not here originally)
+	-- LSP
+	use({ "neovim/nvim-lspconfig" }) -- enable LSP
+	use({ "williamboman/mason.nvim" }) -- simple to use language server installer
+	use({ "williamboman/mason-lspconfig.nvim" })
+	use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
+	--  use "RRethy/vim-illuminate"
 
+	-- Telescope
+	use({ "nvim-telescope/telescope.nvim", tag = "0.1.1" })
 
+	-- Treesitter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use({ "HiPhish/nvim-ts-rainbow2" }) -- rainbow highlighting
+	use({ "nvim-treesitter/playground" })
 
+	-- Git
+	use({ "lewis6991/gitsigns.nvim" })
 
-
- -- Colorschemes
- use { "folke/tokyonight.nvim"}
- use { "lunarvim/darkplus.nvim"}
- use { "lunarvim/colorschemes" }
- use { "rktjmp/lush.nvim" }
- use { "metalelf0/jellybeans-nvim" }
- use { "EdenEast/nightfox.nvim" } -- Packer
- -- Cmp 
- use { "hrsh7th/nvim-cmp" } -- The completion plugin
- use { "hrsh7th/cmp-buffer" } -- buffer completions
- use { "hrsh7th/cmp-path" } -- path completions
- use { "hrsh7th/cmp-cmdline" } -- cmdline completions
- use { "saadparwaiz1/cmp_luasnip" } -- snippet completions
- use { "hrsh7th/cmp-nvim-lsp" } -- lsp configuration
- use { "hrsh7th/cmp-nvim-lua" } -- lua configuration
-
- -- Snippets
- use { "L3MON4D3/LuaSnip", run = "make install_jsregexp" }
- use { "rafamadriz/friendly-snippets" } -- a bunch of snippets to use
-
- -- LSP
- use { "neovim/nvim-lspconfig" } -- enable LSP
- use { "williamboman/mason.nvim" } -- simple to use language server installer
- use { "williamboman/mason-lspconfig.nvim" }
- use { "jose-elias-alvarez/null-ls.nvim" } -- for formatters and linters
- --  use "RRethy/vim-illuminate"
-
- -- Telescope
- use { "nvim-telescope/telescope.nvim", tag = '0.1.1' }
-
- -- Treesitter
- use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
- use { "HiPhish/nvim-ts-rainbow2"} -- rainbow highlighting
- use { "nvim-treesitter/playground" }
-
- -- Git
- use { "lewis6991/gitsigns.nvim" }
-
- --  -- Wakatime
- --  use 'wakatime/vim-wakatime'
+	--  -- Wakatime
+	--  use 'wakatime/vim-wakatime'
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
