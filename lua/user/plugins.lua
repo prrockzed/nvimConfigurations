e14fb5b8270f376daff54e13c95a9595517f4025
prrockzed <prrockzed@pm.me>
2023-03-15 19:20:37 +0530

    Added vim illuminate feature

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -93,7 +93,7 @@ return packer.startup(function(use)
 	use({ "williamboman/mason.nvim" }) -- simple to use language server installer
 	use({ "williamboman/mason-lspconfig.nvim" })
 	use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
-	--  use "RRethy/vim-illuminate"
+	use({ "RRethy/vim-illuminate" })
 
 	-- Telescope
 	use({ "nvim-telescope/telescope.nvim", tag = "0.1.1" })
